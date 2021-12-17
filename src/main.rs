29e9f65dc30bb5cde14b00602f3b42441d3b1047
Zack Hill <zackary.t.hill@gmail.com>
2021-12-17 18:27:59 -0500

    Add solutions for 2021.2

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@
 extern crate nom;
 
 mod year_2021;
-use year_2021::day_01;
+use year_2021::day_02;
 
 use std::time::Instant;
 
 fn main() {
     let start = Instant::now();
 
-    let input = day_01::parse_input();
+    let input = day_02::parse_input();
 
-    println!("Part 1: {}", day_01::solve_part_1(&input));
-    println!("Part 2: {}", day_01::solve_part_2(&input));
+    println!("Part 1: {}", day_02::solve_part_1(&input));
+    println!("Part 2: {}", day_02::solve_part_2(&input));
 
     println!(
         "Time Elapsed: {} ms",
