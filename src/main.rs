558d100986b0b6303e4c68a1ea96dbaccb286659
Zack Hill <zackary.t.hill@gmail.com>
2021-12-23 10:14:52 -0500

    Add solutions for 2021.5

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@
 extern crate nom;
 
 mod year_2021;
-use year_2021::day_04;
+use year_2021::day_05;
 
 use std::time::Instant;
 
 fn main() {
     let start = Instant::now();
 
-    let input = day_04::parse_input();
+    let input = day_05::parse_input();
 
-    println!("Part 1: {}", day_04::solve_part_1(&input));
-    println!("Part 2: {}", day_04::solve_part_2(&input));
+    println!("Part 1: {}", day_05::solve_part_1(&input));
+    println!("Part 2: {}", day_05::solve_part_2(&input));
 
     println!(
         "Time Elapsed: {} ms",
---
+++ src/year_2021/day_05.rs
@@ -0,0 +1,216 @@
+use std::{collections::HashSet, fs::File, io::BufRead, io::BufReader};
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Point {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct LineSegment {
+    start: Point,
+    end: Point,
+}
+
+impl LineSegment {
+    fn is_horizontal(&self) -> bool {
+        self.start.y == self.end.y
+    }
+
+    fn is_vertical(&self) -> bool {
+        self.start.x == self.end.x
+    }
+
+    fn get_points(&self) -> Vec<Point> {
+        let x_increment = LineSegment::get_increment(self.start.x, self.end.x);
+        let y_increment = LineSegment::get_increment(self.start.y, self.end.y);
+
+        let mut x = self.start.x;
+        let mut y = self.start.y;
+
+        let mut points = vec![];
+
+        while x != self.end.x || y != self.end.y {
+            points.push(Point { x, y });
+            x = LineSegment::add(x, x_increment);
+            y = LineSegment::add(y, y_increment);
+        }
+
+        points.push(self.end.clone());
+
+        return points;
+    }
+
+    fn get_increment(left: usize, right: usize) -> isize {
+        if left < right {
+            1
+        } else if left > right {
+            -1
+        } else {
+            0
+        }
+    }
+
+    fn add(u: usize, i: isize) -> usize {
+        if i.is_negative() {
+            u - i.wrapping_abs() as usize
+        } else {
+            u + i as usize
+        }
+    }
+}
+
+pub fn solve_part_1(line_segments: &Vec<LineSegment>) -> usize {
+    let non_diagonal_lines: Vec<&LineSegment> = line_segments
+        .iter()
+        .filter(|l| l.is_horizontal() || l.is_vertical())
+        .collect();
+
+    let max_x = std::cmp::max(
+        non_diagonal_lines.iter().map(|l| l.start.x).max().unwrap(),
+        non_diagonal_lines.iter().map(|l| l.end.x).max().unwrap(),
+    );
+    let max_y = std::cmp::max(
+        non_diagonal_lines.iter().map(|l| l.start.y).max().unwrap(),
+        non_diagonal_lines.iter().map(|l| l.end.y).max().unwrap(),
+    );
+
+    let mut grid = vec![vec![0usize; max_x + 1]; max_y + 1];
+
+    for line in non_diagonal_lines {
+        for point in line.get_points() {
+            grid[point.y][point.x] += 1;
+        }
+    }
+
+    return grid
+        .iter()
+        .flat_map(|r| r.iter())
+        .filter(|&&p| p >= 2)
+        .count();
+}
+
+pub fn solve_part_2(line_segments: &Vec<LineSegment>) -> usize {
+    let max_x = std::cmp::max(
+        line_segments.iter().map(|l| l.start.x).max().unwrap(),
+        line_segments.iter().map(|l| l.end.x).max().unwrap(),
+    );
+    let max_y = std::cmp::max(
+        line_segments.iter().map(|l| l.start.y).max().unwrap(),
+        line_segments.iter().map(|l| l.end.y).max().unwrap(),
+    );
+
+    let mut grid = vec![vec![0usize; max_x + 1]; max_y + 1];
+
+    for line in line_segments {
+        for point in line.get_points() {
+            grid[point.y][point.x] += 1;
+        }
+    }
+
+    return grid
+        .iter()
+        .flat_map(|r| r.iter())
+        .filter(|&&p| p >= 2)
+        .count();
+}
+
+pub fn parse_input() -> Vec<LineSegment> {
+    let file = File::open("src/year_2021/day_05.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut line_segments = vec![];
+
+    for line in reader.lines().map(|l| l.unwrap()) {
+        let values: Vec<usize> = line
+            .split("->")
+            .flat_map(|x| x.split(","))
+            .map(|x| x.trim().parse().unwrap())
+            .collect();
+        let line_segment = LineSegment {
+            start: Point {
+                x: values[0],
+                y: values[1],
+            },
+            end: Point {
+                x: values[2],
+                y: values[3],
+            },
+        };
+        line_segments.push(line_segment);
+    }
+    return line_segments;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_points_horizontal() {
+        let line = LineSegment {
+            start: Point { x: 3, y: 4 },
+            end: Point { x: 5, y: 4 },
+        };
+        let expected = vec![
+            Point { x: 3, y: 4 },
+            Point { x: 4, y: 4 },
+            Point { x: 5, y: 4 },
+        ];
+
+        let actual = line.get_points();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn get_points_horizontal_rev() {
+        let line = LineSegment {
+            end: Point { x: 5, y: 4 },
+            start: Point { x: 3, y: 4 },
+        };
+        let expected = vec![
+            Point { x: 3, y: 4 },
+            Point { x: 4, y: 4 },
+            Point { x: 5, y: 4 },
+        ];
+
+        let actual = line.get_points();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn get_points_vertical() {
+        let line = LineSegment {
+            start: Point { x: 4, y: 3 },
+            end: Point { x: 4, y: 5 },
+        };
+        let expected = vec![
+            Point { x: 4, y: 3 },
+            Point { x: 4, y: 4 },
+            Point { x: 4, y: 5 },
+        ];
+
+        let actual = line.get_points();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn get_points_vertical_rev() {
+        let line = LineSegment {
+            start: Point { x: 4, y: 5 },
+            end: Point { x: 4, y: 3 },
+        };
+        let expected = vec![
+            Point { x: 4, y: 5 },
+            Point { x: 4, y: 4 },
+            Point { x: 4, y: 3 },
+        ];
+
+        let actual = line.get_points();
+
+        assert_eq!(expected, actual);
+    }
+}
