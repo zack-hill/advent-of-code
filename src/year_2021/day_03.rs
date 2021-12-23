3ef8d11359bfab7bf66db794620d19f94fd24ed7
Zack Hill <zackary.t.hill@gmail.com>
2021-12-22 22:09:55 -0500

    Greatly simplify solution for part 1 of 2021.3

--- src/year_2021/day_03.rs
+++ src/year_2021/day_03.rs
@@ -1,52 +1,51 @@
-use itertools::GroupBy;
-use itertools::Itertools;
 use std::{fs::File, io::BufRead, io::BufReader};
 
-pub fn solve_part_1(input: &Vec<Vec<char>>) -> u32 {
-    let length = input[0].len();
-    let gamma_rate = (0..length)
-        .map(|i| {
-            input
-                .iter()
-                .map(|x| x[i])
-                .sorted()
-                .group_by(|&x| x)
-                .into_iter()
-                .map(|(key, group)| (key, group.count()))
-                .max_by_key(|&(_, count)| count)
-                .unwrap()
-                .0
-        })
-        .collect::<String>();
-    println!("{:?}", gamma_rate);
-    let gamma_rate = u16::from_str_radix(&gamma_rate, 2).unwrap();
-    let epsilon_rate = !gamma_rate;
-    // TODO: This is awful and should be rewritten.
-    let epsilon_rate = epsilon_rate
-        .to_be_bytes()
-        .iter()
-        .map(|b| format!("{:b}", b))
-        .join("")
-        .chars()
-        .skip(4)
-        .take(length)
-        .collect::<String>();
-    println!("{:?}", epsilon_rate);
-    let epsilon_rate = u16::from_str_radix(&epsilon_rate, 2).unwrap();
-    println!("{:?}", gamma_rate);
-    println!("{:?}", epsilon_rate);
+type BitVec = Vec<bool>;
+
+pub fn solve_part_1(input: &Vec<BitVec>) -> u32 {
+    let bit_count = input[0].len();
+
+    // Find most common bit for each position
+    let gamma_rate_bits = (0..bit_count)
+        .map(|i| input.iter().filter(|x| x[i] == true).count() > (input.len() / 2))
+        .collect::<BitVec>();
+    let gamma_rate = to_u32(&gamma_rate_bits);
+    //println!("Gamma Rate Bits: {:?}", gamma_rate_bits);
+    println!("Gamma Rate: {:?}", gamma_rate);
+
+    // Negate to find least common bit for each position
+    let epsilon_rate_bits = negate(&gamma_rate_bits);
+    let epsilon_rate = to_u32(&epsilon_rate_bits);
+    //println!("Epsilon Rate Bits: {:?}", epsilon_rate_bits);
+    println!("Epsilon Rate: {:?}", epsilon_rate);
+
     return gamma_rate as u32 * epsilon_rate as u32;
 }
 
-pub fn solve_part_2(input: &Vec<Vec<char>>) -> usize {
+pub fn solve_part_2(input: &Vec<BitVec>) -> usize {
     return 0;
 }
 
-pub fn parse_input() -> Vec<Vec<char>> {
+fn to_u32(bits: &BitVec) -> u32 {
+    return bits.iter().rev().enumerate().fold(0 as u32, |acc, (i, x)| {
+        acc + (*x as u32) * u32::pow(2, i as u32)
+    });
+}
+
+fn negate(bits: &BitVec) -> BitVec {
+    return bits.iter().map(|x| !x).collect();
+}
+
+pub fn parse_input() -> Vec<BitVec> {
     let file = File::open("src/year_2021/day_03.txt").unwrap();
     let reader = BufReader::new(file);
-    return reader
-        .lines()
-        .map(|line| line.unwrap().chars().collect())
-        .collect();
+
+    let mut bits_collection = vec![];
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let bits = line.chars().map(|c| c == '1').collect();
+        bits_collection.push(bits);
+    }
+    return bits_collection;
 }
