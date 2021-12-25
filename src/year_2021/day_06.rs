ba88fcc66cf37a671cee0b456fccfdf2ddf54df8
Zack Hill <zackary.t.hill@gmail.com>
2021-12-25 14:16:40 -0500

    Add solution for part 2 of 2021.6

--- src/year_2021/day_06.rs
+++ src/year_2021/day_06.rs
@@ -1,58 +1,42 @@
 use std::{fs::File, io::BufRead, io::BufReader};
 
-pub fn solve_part_1(ages: &Vec<u32>) -> usize {
-    let mut ages = ages.clone();
-
-    let new_fish_age = 8;
-    let recovery_time = 6;
-    let days = 80;
-
-    for _ in 0..days {
-        let mut new_fish = vec![];
-        for age in ages.iter_mut() {
-            if *age == 0 {
-                new_fish.push(new_fish_age);
-                *age = recovery_time;
-            } else {
-                *age -= 1;
-            }
-        }
-        ages.append(&mut new_fish);
-    }
-
-    return ages.len();
+pub fn solve_part_1(timers: &[u64; 9]) -> u64 {
+    simulate(&mut timers.clone(), 80)
 }
 
-pub fn solve_part_2(ages: &Vec<u32>) -> usize {
-    let mut ages = ages.clone();
-
-    let new_fish_age = 8;
-    let recovery_time = 6;
-    let days = 256;
-
-    for _ in 0..days {
-        let mut new_fish = vec![];
-        for age in ages.iter_mut() {
-            if *age == 0 {
-                new_fish.push(new_fish_age);
-                *age = recovery_time;
-            } else {
-                *age -= 1;
-            }
-        }
-        ages.append(&mut new_fish);
-    }
-
-    return ages.len();
+pub fn solve_part_2(timers: &[u64; 9]) -> u64 {
+    simulate(&mut timers.clone(), 256)
 }
 
-pub fn parse_input() -> Vec<u32> {
+fn simulate(timers: &mut [u64; 9], days: u32) -> u64 {
+    for _ in 0..days {
+        let reproducing_count = timers[0];
+        for timer_day in 1..=8 {
+            timers[timer_day - 1] = timers[timer_day];
+        }
+        timers[6] += reproducing_count;
+        timers[8] = reproducing_count;
+    }
+
+    return timers.iter().sum();
+}
+
+pub fn parse_input() -> [u64; 9] {
     let file = File::open("src/year_2021/day_06.txt").unwrap();
     let mut reader = BufReader::new(file);
 
     let mut line = String::new();
     reader.read_line(&mut line).unwrap();
 
-    let ages = line.split(',').map(|age| age.parse().unwrap()).collect();
-    return ages;
+    let mut timer_groups = [0u64; 9];
+    let timers: Vec<usize> = line
+        .split(',')
+        .map(|timer| timer.parse().unwrap())
+        .collect();
+
+    for timer in timers {
+        timer_groups[timer] += 1;
+    }
+
+    return timer_groups;
 }
