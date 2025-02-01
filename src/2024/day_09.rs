52fbe5329116baa94780e3635d3fb1c001ed93e1
Zack Hill <zackary.t.hill@gmail.com>
2025-02-01 11:08:25 -0500

    Add solution for part 2

--- src/2024/day_09.rs
+++ src/2024/day_09.rs
@@ -80,40 +80,179 @@ impl Solver {
         return working_memory;
     }
 
-    fn calculate_checksum(memory: &Vec<MemoryBlock>) -> usize {
-        memory
-            .iter()
-            .enumerate()
-            .map(|(index, memory_block)| match memory_block {
-                MemoryBlock::FreeSpace => 0,
-                MemoryBlock::File(id) => id * index,
-            })
-            .sum()
-    }
+    fn defrag_2(&self) -> Vec<MemoryBlock> {
+        let mut working_memory = self.memory.clone();
 
-    fn print(memory: &Vec<MemoryBlock>) {
-        for block in memory.iter() {
-            print!(
-                "{}",
-                match block {
-                    MemoryBlock::File(id) => id.to_string(),
-                    MemoryBlock::FreeSpace => ".".to_string(),
+        // Find id of last memory block. This will be the max id of all file blocks in the memory.
+        // This value will be decremented during the defrag process to ensure blocks are not moved more than once.
+        let mut max_id = *working_memory
+            .iter()
+            .filter_map(|block| {
+                if let MemoryBlock::File(id) = block {
+                    Some(id)
+                } else {
+                    None
                 }
-            );
+            })
+            .last()
+            .unwrap();
+
+        let mut free_space_sections = find_free_space_sections(&working_memory);
+
+        loop {
+            if let Some((start_index, length, block_id)) =
+                find_memory_block_to_move(&working_memory, max_id)
+            {
+                if let Some(free_space_index) =
+                    free_space_sections.iter().position(|(_, l)| *l >= length)
+                {
+                    let (free_space_start, free_space_length) =
+                        free_space_sections[free_space_index];
+
+                    if free_space_start > start_index {
+                        // println!(
+                        //     "Not moving {}, no free spaces left of {}",
+                        //     block_id, free_space_start
+                        // );
+                    } else {
+                        // println!(
+                        //     "moving {} from {} to {}",
+                        //     block_id, start_index, free_space_start
+                        // );
+
+                        // Replace free space block with file
+                        for i in free_space_start..free_space_start + length {
+                            working_memory[i] = MemoryBlock::File(block_id);
+                        }
+
+                        // Replace file block with free space
+                        for i in start_index..start_index + length {
+                            working_memory[i] = MemoryBlock::FreeSpace;
+                        }
+
+                        // Shrink free space accordingly. No need to add new gap created at the end
+                        // as the algorithm won't try to place a file there.
+                        let updated_free_space_start = free_space_start + length;
+                        let updated_free_space_length = free_space_length - length;
+                        free_space_sections[free_space_index] =
+                            (updated_free_space_start, updated_free_space_length);
+                    }
+                }
+                if max_id > 0 {
+                    max_id -= 1;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
         }
-        println!("")
+
+        return working_memory;
     }
+}
+
+fn find_free_space_sections(memory: &Vec<MemoryBlock>) -> Vec<(usize, usize)> {
+    let mut free_space: Vec<(usize, usize)> = Vec::new();
+    let mut section_start: Option<usize> = None;
+    for (i, memory_block) in memory.iter().enumerate() {
+        match memory_block {
+            MemoryBlock::FreeSpace => {
+                if section_start == None {
+                    section_start = Some(i);
+                }
+            }
+            _ => {
+                if let Some(start_index) = section_start {
+                    free_space.push((start_index, i - start_index));
+                    section_start = None;
+                }
+            }
+        }
+    }
+    // handle scenario where memory ends with free space
+    if let Some(start_index) = section_start {
+        free_space.push((start_index, memory.len() - start_index));
+    }
+    return free_space;
+}
+
+// Returns (start_index, length, id)
+fn find_memory_block_to_move(
+    memory: &Vec<MemoryBlock>,
+    max_id: usize,
+) -> Option<(usize, usize, usize)> {
+    let mut end_index: Option<usize> = None;
+    let mut block_id: Option<usize> = None;
+
+    for (i, memory_block) in memory.iter().enumerate().rev() {
+        match memory_block {
+            MemoryBlock::FreeSpace => {
+                if let Some(end_index) = end_index {
+                    let start_index = i + 1;
+                    let length = (end_index - start_index) + 1;
+                    return Some((start_index, length, block_id.unwrap()));
+                }
+            }
+            MemoryBlock::File(id) => {
+                if let Some(block_id) = block_id {
+                    if block_id == *id {
+                        continue;
+                    }
+                    let start_index = i + 1;
+                    let length = (end_index.unwrap() - start_index) + 1;
+                    return Some((start_index, length, block_id));
+                } else {
+                    if *id <= max_id {
+                        end_index = Some(i);
+                        block_id = Some(*id);
+                    }
+                }
+            }
+        }
+    }
+    if let Some(end_index) = end_index {
+        let length = end_index;
+        return Some((0, length, block_id.unwrap()));
+    }
+    return None;
+}
+
+fn calculate_checksum(memory: &Vec<MemoryBlock>) -> usize {
+    memory
+        .iter()
+        .enumerate()
+        .map(|(index, memory_block)| match memory_block {
+            MemoryBlock::FreeSpace => 0,
+            MemoryBlock::File(id) => id * index,
+        })
+        .sum()
+}
+
+fn print(memory: &Vec<MemoryBlock>) {
+    for block in memory.iter() {
+        print!(
+            "{}",
+            match block {
+                MemoryBlock::File(id) => id.to_string(),
+                MemoryBlock::FreeSpace => ".".to_string(),
+            }
+        );
+    }
+    println!("")
 }
 
 impl AoCSolver for Solver {
     fn solve_part_1(&self) -> String {
         let defragged = self.defrag();
-        let checksum = Solver::calculate_checksum(&defragged);
+        let checksum = calculate_checksum(&defragged);
         return checksum.to_string();
     }
 
     fn solve_part_2(&self) -> String {
-        return "".to_string();
+        let defragged = self.defrag_2();
+        let checksum = calculate_checksum(&defragged);
+        return checksum.to_string();
     }
 }
 
@@ -125,8 +264,43 @@ mod tests {
     fn defrag_example() {
         let solver = Solver::from_dense_format("2333133121414131402");
         let defragged = solver.defrag();
-        Solver::print(&defragged);
-        let checksum = Solver::calculate_checksum(&defragged);
+        print(&defragged);
+        let checksum = calculate_checksum(&defragged);
         assert_eq!(checksum, 1928);
+    }
+
+    #[test]
+    fn defrag_2_example() {
+        let solver = Solver::from_dense_format("2333133121414131402");
+        let defragged = solver.defrag_2();
+        print(&defragged);
+        let checksum = calculate_checksum(&defragged);
+        assert_eq!(checksum, 2858);
+    }
+
+    #[test]
+    fn find_free_space_sections_finds_expected_sections() {
+        let solver = Solver::from_dense_format("2333133121414131402");
+        let sections = find_free_space_sections(&solver.memory);
+        let expected = vec![
+            (2, 3),
+            (8, 3),
+            (12, 3),
+            (18, 1),
+            (21, 1),
+            (26, 1),
+            (31, 1),
+            (35, 1),
+        ];
+        assert_eq!(sections, expected);
+    }
+
+    #[test]
+    fn find_memory_block_to_move_test() {
+        let solver = Solver::from_dense_format("2333133121414131402");
+        let (start_index, length, id) = find_memory_block_to_move(&solver.memory, 7).unwrap();
+        assert_eq!(start_index, 32);
+        assert_eq!(length, 3);
+        assert_eq!(id, 7);
     }
 }
